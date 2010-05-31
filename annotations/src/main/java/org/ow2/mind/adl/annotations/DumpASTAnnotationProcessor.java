6aee693acae3894f1116a1ebf68b22fe13553f12
Matthieu Anne <matthieu.anne@orange-ftgroup.co>
2010-05-31 16:02:57 +0000

    Add a separate annotations module to gather annotations.  - Add a @Wrap annotation to specify in an ADL that the wrapping code of a server interface will be automatically generated.  - Add a @VarArgsDual(<functionName>) to specify the name of a variadic dual function in an IDL (useful to wrap interface with variadic methods).
     Update the associate documentation (user guide).
    
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/mindc/trunk@952 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

---
+++ annotations/src/main/java/org/ow2/mind/adl/annotations/DumpASTAnnotationProcessor.java
@@ -0,0 +1,177 @@
+/**
+ * Copyright (C) 2010 France Telecom
+ *
+ * This file is part of "Mind Compiler" is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: mind@ow2.org
+ *
+ * Authors: Matthieu ANNE
+ * Contributors:
+ */
+
+package org.ow2.mind.adl.annotations;
+
+import java.util.Map;
+
+import org.objectweb.fractal.adl.ADLException;
+import org.objectweb.fractal.adl.Definition;
+import org.objectweb.fractal.adl.Node;
+import org.objectweb.fractal.adl.interfaces.Interface;
+import org.objectweb.fractal.adl.interfaces.InterfaceContainer;
+import org.ow2.mind.adl.annotation.ADLLoaderPhase;
+import org.ow2.mind.adl.annotation.AbstractADLLoaderAnnotationProcessor;
+import org.ow2.mind.adl.ast.ASTHelper;
+import org.ow2.mind.adl.ast.Binding;
+import org.ow2.mind.adl.ast.BindingContainer;
+import org.ow2.mind.adl.ast.Component;
+import org.ow2.mind.adl.ast.ComponentContainer;
+import org.ow2.mind.adl.ast.MindInterface;
+import org.ow2.mind.annotation.Annotation;
+
+/**
+ * @author Matthieu ANNE
+ */
+public class DumpASTAnnotationProcessor extends
+	AbstractADLLoaderAnnotationProcessor {
+
+    private static void showComponents(final Definition definition,
+	    final int depth) {
+	String prf = "  ";
+
+	for (int i = 0; i < depth; i++)
+	    prf = prf + "   ";
+
+	System.out.println(prf + "Definition source: "
+		+ definition.astGetSource());
+
+	final Interface[] interfaces = ((InterfaceContainer) definition)
+		.getInterfaces();
+	if (interfaces.length == 0) {
+	    System.out.println(prf + "No interface");
+	} else {
+	    System.out
+		    .println(prf + "Interface quantity: " + interfaces.length);
+	}
+	for (int i = 0; i < interfaces.length; i++) {
+	    final MindInterface itf = (MindInterface) interfaces[i];
+	    System.out.print(prf + "Interface[" + i + "] is " + itf.getName()
+		    + " with role " + itf.getRole() + ", signature: "
+		    + itf.getSignature());
+	    if (itf.getCardinality() != null) {
+		System.out.print(", cardinality: " + itf.getCardinality());
+	    }
+	    if (itf.getContingency() != null) {
+		System.out.print(", contingency: " + itf.getContingency());
+	    }
+	    if (itf.getNumberOfElement() != null) {
+		System.out.print(", number of element: "
+			+ itf.getNumberOfElement());
+	    }
+	    System.out.println();
+	}
+
+	if (ASTHelper.isComposite(definition)) {
+	    final Component[] subComponents = ((ComponentContainer) definition)
+		    .getComponents();
+	    System.out.print(prf + "Subcomponent quantity: "
+		    + subComponents.length + " (" + subComponents[0].getName());
+	    for (int i = 1; i < subComponents.length; i++) {
+		System.out.print(", " + subComponents[i].getName());
+	    }
+	    System.out.println(")");
+
+	    final Binding[] bindings = ((BindingContainer) definition)
+		    .getBindings();
+	    if (bindings.length == 0) {
+		System.out.println(prf + "No binding");
+	    } else {
+		System.out
+			.println(prf + "Binding quantity: " + bindings.length);
+	    }
+	    for (int i = 0; i < bindings.length; i++) {
+		final Binding binding = bindings[i];
+		System.out.print(prf + "Binding #" + i + " is from "
+			+ binding.getFromComponent() + "."
+			+ binding.getFromInterface());
+		if (binding.getFromInterfaceNumber() != null) {
+		    System.out.print("[" + binding.getFromInterfaceNumber()
+			    + "]");
+		}
+		System.out.print(" to " + binding.getToComponent() + "."
+			+ binding.getToInterface());
+		if (binding.getToInterfaceNumber() != null) {
+		    System.out
+			    .print("[" + binding.getToInterfaceNumber() + "]");
+		}
+		System.out.println();
+	    }
+
+	    for (int i = 0; i < subComponents.length; i++) {
+		final Component subComponent = subComponents[i];
+		try {
+		    System.out.println(prf
+			    + "Component #"
+			    + i
+			    + ": "
+			    + subComponent.getName()
+			    + " ("
+			    + ASTHelper.getResolvedComponentDefinition(
+				    subComponent, null, null).getName() + ")");
+		} catch (final ADLException e1) {
+		    // TODO Auto-generated catch block
+		    e1.printStackTrace();
+		}
+		try {
+		    showComponents(ASTHelper.getResolvedDefinition(subComponent
+			    .getDefinitionReference(), null, null), depth + 1);
+		} catch (final ADLException e) {
+		    // TODO Auto-generated catch block
+		    e.printStackTrace();
+		}
+	    }
+
+	} else if (ASTHelper.isPrimitive(definition)) {
+
+	}
+    }
+
+    private static void showDefinitionContent(final Definition definition) {
+	System.out.println("Showing content of current definition: "
+		+ definition.getName() + ";\n");
+	showComponents(definition, 0);
+
+	System.out
+		.println("\n\n---------------------------------------------------------------\n\n");
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.ow2.mind.adl.annotation.ADLLoaderAnnotationProcessor#processAnnotation
+     * (org.ow2.mind.annotation.Annotation, org.objectweb.fractal.adl.Node,
+     * org.objectweb.fractal.adl.Definition,
+     * org.ow2.mind.adl.annotation.ADLLoaderPhase, java.util.Map)
+     */
+    public Definition processAnnotation(final Annotation annotation,
+	    final Node node, final Definition definition,
+	    final ADLLoaderPhase phase, final Map<Object, Object> context)
+	    throws ADLException {
+	assert annotation instanceof DumpAST;
+	showDefinitionContent(definition);
+	return null;
+    }
+
+}
---
+++ annotations/src/main/java/org/ow2/mind/adl/annotations/WrapAnnotationProcessor.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (C) 2010 France Telecom
+ *
+ * This file is part of "Mind Compiler" is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: mind@ow2.org
+ *
+ * Authors: Matthieu ANNE
+ * Contributors:
+ */
+
+package org.ow2.mind.adl.annotations;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.antlr.stringtemplate.StringTemplate;
+import org.objectweb.fractal.adl.ADLException;
+import org.objectweb.fractal.adl.Definition;
+import org.objectweb.fractal.adl.Node;
+import org.objectweb.fractal.adl.error.GenericErrors;
+import org.objectweb.fractal.adl.interfaces.Interface;
+import org.objectweb.fractal.adl.types.TypeInterface;
+import org.objectweb.fractal.adl.types.TypeInterfaceUtil;
+import org.ow2.mind.adl.annotation.ADLLoaderPhase;
+import org.ow2.mind.adl.annotation.AbstractADLLoaderAnnotationProcessor;
+import org.ow2.mind.adl.ast.ASTHelper;
+import org.ow2.mind.adl.ast.ImplementationContainer;
+import org.ow2.mind.adl.ast.Source;
+import org.ow2.mind.adl.idl.InterfaceDefinitionDecorationHelper;
+import org.ow2.mind.annotation.Annotation;
+import org.ow2.mind.annotation.AnnotationErrors;
+import org.ow2.mind.annotation.AnnotationHelper;
+import org.ow2.mind.idl.annotations.VarArgsDual;
+import org.ow2.mind.idl.ast.InterfaceDefinition;
+import org.ow2.mind.idl.ast.Method;
+
+/**
+ * @author Matthieu ANNE
+ */
+public class WrapAnnotationProcessor
+    extends
+      AbstractADLLoaderAnnotationProcessor {
+
+  protected static final String IDL2CPLWRAPPER_TEMPLATE_NAME = "st.interfaceWrapping.IDL2CPLWRAPPER";
+
+  public Definition processAnnotation(final Annotation annotation,
+      final Node node, final Definition definition, final ADLLoaderPhase phase,
+      final Map<Object, Object> context) throws ADLException {
+    assert annotation instanceof Wrap;
+
+    if (ASTHelper.isPrimitive(definition)) {
+      final Interface itf = (Interface) node;
+      if (TypeInterfaceUtil.isServer(itf)) {
+        final InterfaceDefinition itfDef = InterfaceDefinitionDecorationHelper
+            .getResolvedInterfaceDefinition((TypeInterface) itf, idlLoaderItf,
+                context);
+
+        final Map<String, String> dualMeths = new HashMap<String, String>();
+
+        for (final Method meth : itfDef.getMethods()) {
+          final VarArgsDual methAnnotation = AnnotationHelper.getAnnotation(
+              meth, VarArgsDual.class);
+          if (methAnnotation != null) {
+            dualMeths.put(meth.getName(), (methAnnotation).value);
+          }
+        }
+
+        // TODO might need a #line in generated file to find error in
+        // source file
+        // NodeErrorLocator sourceInfo = new NodeErrorLocator(node);
+
+        final StringTemplate st = getTemplate(IDL2CPLWRAPPER_TEMPLATE_NAME,
+            "cplFile");
+        st.setAttribute("idl", itfDef);
+        st.setAttribute("itfName", itf.getName());
+        st.setAttribute("dualMeths", dualMeths);
+        // st.setAttribute("sourceInfo", sourceInfo);
+
+        try {
+          final Source src = (Source) nodeFactoryItf.newNode("source",
+              Source.class.getName());
+          src.setCCode(st.toString());
+          ((ImplementationContainer) definition).addSource(src);
+        } catch (final ClassNotFoundException e) {
+          throw new ADLException(GenericErrors.INTERNAL_ERROR, e);
+        }
+      } else {
+        throw new ADLException(AnnotationErrors.INVALID_ANNOTATION, node,
+            "@Wrap. Client's interfaces cannot be wrapped.");
+      }
+    } else {
+      throw new ADLException(AnnotationErrors.INVALID_ANNOTATION, node,
+          "@Wrap applied to interface " + ((Interface) node).getName()
+              + ".\n Composite's interfaces cannot be wrapped: "
+              + definition.astGetSource());
+    }
+    return null;
+  }
+}
