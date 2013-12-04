048a36743d52636a7cff5529efbeb46ccc498409
Seyvoz Stéphane <stephaneseyvoz@gmail.com>
2013-12-04 16:01:49 +0100

    Completely new strategy for Singleton's PRIVATE data. Fixed issue: Preprocessing of user C code + membrane pre-processing resulted in duplicate PRIVATE struct for the definition: GCC understood the elements were the same but IAR was strict about duplicate structs errors. Solution/Strategy: New behavior was inspired by the non-Singleton code generation, using typedefs instead of direct struct use, guards, and component private data initialization through a dedicated macro. Tests were fixed accordingly.

--- cpl-preproc/src/test/java/org/ow2/mind/preproc/AbstractTestMPP.java
+++ cpl-preproc/src/test/java/org/ow2/mind/preproc/AbstractTestMPP.java
@@ -177,6 +177,16 @@ public class AbstractTestMPP {
     command.addDefine("COMPONENT_NAME", dirName.replace('-', '_'));
     if (singleton) command.addDefine("SINGLETON");
 
+    /* Duplicate-definition bug fix for some families of compilers (such as IAR) */
+    if (singleton) {
+      final String dataDefName = "private_data_def.c";
+      final URL dataDefURL = getClass().getClassLoader().getResource(
+          dataDefName);
+      if (dataDefURL == null)
+        throw new Exception("Can't find file " + dataDefName);
+      command.addIncludeFile(new File(dataDefURL.toURI()));
+    }
+
     return command;
   }
 
