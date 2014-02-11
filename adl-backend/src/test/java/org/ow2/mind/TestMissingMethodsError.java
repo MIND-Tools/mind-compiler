ff9af39882ee6fa8d0e92f3250ee6eb84bc9cb1f
Seyvoz Stéphane <stephaneseyvoz@gmail.com>
2014-02-11 16:08:52 +0100

    Fix copyright See: https://github.com/Mind4SE/mind-compiler/commit/4b4658364bf9542a0202ba7d49369b501248b082#adl-backend-src-test-java-org-ow2-mind-testmissingmethodserror-java-P2

--- adl-backend/src/test/java/org/ow2/mind/TestMissingMethodsError.java
+++ adl-backend/src/test/java/org/ow2/mind/TestMissingMethodsError.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013 STMicroelectronics
+ * Copyright (C) 2013 Schneider-Electric
  *
  * This file is part of "Mind Compiler" is free software: you can redistribute 
  * it and/or modify it under the terms of the GNU Lesser General Public License 
