80ef2f7c2230c8bf64b7d85e9613dcfc59488b41
Seyvoz Stéphane <stephaneseyvoz@gmail.com>
2014-04-01 16:27:33 +0200

    Fix ImportIDL test to use the right Bootstrap and Main (from common/).

--- annotations/src/test/resources/functional/importidl/relay.c
+++ annotations/src/test/resources/functional/importidl/relay.c
@@ -1,13 +1,8 @@
 
-// -----------------------------------------------------------------------------
-// Implementation of the primitive test.useidl.initf.Relay.
-// -----------------------------------------------------------------------------
-
-
 int METH(relayServiceItf, relayInterfacePointer)(importidl_TargetService targetItf) {
 	int res = 1;
 
-	// bind our client interface to the one given to us
+	/* bind our client interface to the one given to us */
 	BIND_MY_INTERFACE(targetServiceItf, targetItf);
 	if (IS_BOUND(targetServiceItf))
 		res = CALL(targetServiceItf, getResult)();
