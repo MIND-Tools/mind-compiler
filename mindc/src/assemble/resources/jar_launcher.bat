e167c7be4219ea4c3a5cf40bfb2c95d6f6e306fe
Francois DELOYE <francois.deloye@non.schneider-electric.com>
2014-07-04 13:50:12 +0200

    Added jar_launcher as generic java launcher Modified mindc scripts

---
+++ mindc/src/assemble/resources/jar_launcher.bat
@@ -0,0 +1,128 @@
+@REM Copyright (C) 2009 Sogeti High-Tech
+@REM Copyright (C) 2014 Schneider Electric
+@REM
+@REM  This file is part of "Mind Compiler" is free software: you can redistribute 
+@REM  it and/or modify it under the terms of the GNU Lesser General Public License 
+@REM  as published by the Free Software Foundation, either version 3 of the 
+@REM  License, or (at your option) any later version.
+@REM 
+@REM  This program is distributed in the hope that it will be useful, but WITHOUT 
+@REM  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+@REM  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+@REM  details.
+@REM 
+@REM  You should have received a copy of the GNU Lesser General Public License
+@REM  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@REM 
+@REM  Contact: mind@ow2.org
+@REM 
+@REM  Authors: Edine Coly
+@REM  Contributors: Schneider Electric Mind4SE
+@REM -----------------------------------------------------------------------------
+
+@REM Generic launcher for Mind tools
+@REM
+@REM Required parameters:
+@REM --------------------
+@REM   1st arg: Java class
+@REM   Other args: Parameters for command line
+@REM
+@REM Optional ENV vars:
+@REM ------------------
+@REM   JAVA_HOME - location of a JRE home dir
+@REM
+
+@echo off
+setlocal
+@REM ==== CHECK JAVA_HOME ===
+if "%JAVA_HOME%" == "" goto NoJHome
+
+@REM ==== CHECK JAVA_HOME_EXE ===
+echo __JVH:%JAVA_HOME%
+"%JAVA_HOME%\bin\java.exe" -? > nul 2>&1
+set JAVACMD="%JAVA_HOME%\bin\java.exe"
+echo __JVC:%JAVA_CMD%
+if ERRORLEVEL 0 goto OkJava
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory.
+echo JAVA_HOME = "%JAVA_HOME%"
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation
+echo.
+goto error
+
+:NoJHome
+@REM ==== CHECK JAVA IS ACCESSIBLE FROM PATH ====
+java.exe -? > nul 2>&1
+set JAVACMD="java.exe"
+if ERRORLEVEL 0 goto OkJava
+ 
+echo.
+echo ERROR: Java not found in your system.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation
+echo.
+goto error
+
+:OkJava
+@REM use the batch path to determine MIND_HOME
+pushd %~dp0..\
+set MIND_HOME=%cd%
+popd
+
+set MIND_RUNTIME=%MIND_HOME%\runtime
+set MIND_LIB=%MIND_HOME%\lib
+set MIND_EXT=%MIND_HOME%\ext
+if not "%MIND_CLASSPATH%" == "" set MIND_CLASSPATH=%MIND_CLASSPATH%;
+
+for /r "%MIND_LIB%\" %%i in (*.jar) do (
+  set VarTmp=%%~fnxi;& call :concat
+  )
+for /r "%MIND_EXT%\" %%i in (*.jar) do (
+  set VarTmp=%%~fnxi;& call :concat
+  )
+
+goto :run
+
+:concat
+set MIND_CLASSPATH=%MIND_CLASSPATH%%VarTmp%
+goto :eof
+
+:run
+@REM Split: 1st arg->CMD
+set CMD=%1
+
+@REM Split: other args->PARAMS (remove in loop 1st character until space is found)
+set PARAMS=%*
+:loop
+if "%PARAMS:~0,1%" neq " " (
+  set PARAMS=%PARAMS:~1%
+  goto :loop )
+
+echo *=%*
+echo CMD=%CMD%
+echo PARAMS=%PARAMS%
+@rem echo MIND_HOME=%MIND_HOME%
+@rem echo MIND_LIB=%MIND_LIB%
+@rem echo MIND_EXT=%MIND_EXT%
+@rem echo MIND_RUNTIME=%MIND_RUNTIME%
+@rem echo MIND_CLASSPATH=%MIND_CLASSPATH%
+
+echo %CMD% | findstr ".*mind.Launcher" > nul 2>&1
+if ERRORLEVEL 1 (
+  echo %CMD% | findstr ".*unit.Launcher" > nul 2>&1
+)
+if NOT ERRORLEVEL 1 (
+  echo %JAVACMD% -classpath %MIND_CLASSPATH% %PARAMS% -src-path=%MIND_RUNTIME%
+  %JAVACMD% -classpath %MIND_CLASSPATH% %PARAMS% -src-path=%MIND_RUNTIME%
+) else (
+  echo %JAVACMD% -classpath %MIND_CLASSPATH% %PARAMS%
+  %JAVACMD% -classpath %MIND_CLASSPATH% %PARAMS%
+)
+
+goto :eof
+
+
+:error
+@REM If not set, force ERRORLEVEL to 1
+if NOT ERRORLEVEL 1 exit /b 1
--- mindc/src/assemble/resources/mindc.bat
+++ mindc/src/assemble/resources/mindc.bat
@@ -1,3 +1,6 @@
+
+@REM  Copyright (C) 2014 Schneider Electric
+@REM
 @REM  This file is part of "Mind Compiler" is free software: you can redistribute 
 @REM  it and/or modify it under the terms of the GNU Lesser General Public License 
 @REM  as published by the Free Software Foundation, either version 3 of the 
@@ -13,92 +16,23 @@
 @REM 
 @REM  Contact: mind@ow2.org
 @REM 
-@REM  Authors: Edine Coly (edine.coly@mail.sogeti.com)
+@REM  Authors: Schneider Electric Mind4SE
 @REM  Contributors: 
 @REM -----------------------------------------------------------------------------
-@REM Mind Compiler batch script ${project.version}
-@REM
-@REM Required ENV vars:
-@REM ------------------
-@REM   JAVA_HOME - location of a JDK home dir
 @REM
 @REM Optional ENV vars
 @REM -----------------
-@REM   MIND_HOME - location of mind's installed home dir
 @REM   MIND_OPTS - parameters passed to the Java VM running the mind compiler
 @REM     e.g. to specify logging levels, use
 @REM       set MIND_OPTS=-Ddefault.console.level=FINE -Ddefault.file.level=FINER
 @REM   See documentation for more detail on logging system.
 
 @echo off
+setlocal 
 
-@REM ==== CHECK JAVA_HOME ===
-if not "%JAVA_HOME%" == "" goto OkJHome
-echo.
-echo ERROR: JAVA_HOME not found in your environment.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation
-echo.
-goto error
-
-:OkJHome
-@REM ==== CHECK JAVA_HOME_EXE ===
-if exist "%JAVA_HOME%\bin\java.exe" goto OkJHomeExe
-
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory.
-echo JAVA_HOME = "%JAVA_HOME%"
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation
-echo.
-goto error
-
-:OkJHomeExe
-@REM ==== CHECK MIND_HOME ===
-@REM use the batch path to determine MIND_HOME if not defined.
-pushd %~dp0..\
-set MINDC_ROOT=%cd%
-popd
-
-if "%MIND_HOME%" == "" set MIND_HOME=%MINDC_ROOT%
-
-@REM MIND_HOME defined and different from batch path, use it but warn the user
-if /i "%MIND_HOME%" == "%MINDC_ROOT%" goto endInit
-echo.
-echo WARNING: Using environment variable MIND_HOME which is different from mindc.bat location
-echo MIND_HOME          = %MIND_HOME% 
-echo mindc.bat location = %MINDC_ROOT%
-echo.
-
-:endInit
-
-setlocal
-set MIND_CMD_LINE_ARGS=%*
-set MIND_RUNTIME=%MIND_HOME%/runtime
-set MIND_LIB=%MIND_HOME%/lib
-set MIND_EXT=%MIND_HOME%/ext
+@REM Launcher class name
 set LAUNCHER=org.ow2.mind.Launcher
-set MIND_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
-if not "%MIND_CLASSPATH%" == "" set MIND_CLASSPATH=%MIND_CLASSPATH%;
 
-for /r "%MIND_LIB%\" %%i in (*.jar) do (
-    set VarTmp=%%~fnxi;& call :concat
-    )
-for /r "%MIND_EXT%\" %%i in (*.jar) do (
-    set VarTmp=%%~fnxi;& call :concat
-    )
+@rem echo %~dp0\jar_launcher.bat %LAUNCHER% %MIND_OPTS% -Dmindc.launcher.name=mindc %LAUNCHER% %*
+%~dp0\jar_launcher.bat %LAUNCHER% %MIND_OPTS% -Dmindc.launcher.name=mindc %LAUNCHER% %*
 
-goto :runMind
-:concat
-set MIND_CLASSPATH=%MIND_CLASSPATH%%VarTmp%
-goto :eof
-
-:runMind
-%MIND_JAVA_EXE% -classpath %MIND_CLASSPATH% %MIND_OPTS% -Dmindc.launcher.name=mindc %LAUNCHER% -src-path=%MIND_RUNTIME% %MIND_CMD_LINE_ARGS%
-
-
-:error
-@echo off
-if "%OS%"=="Windows_NT" @endlocal
-if "%OS%"=="WINNT" @endlocal
-(set ERROR_CODE=1)
